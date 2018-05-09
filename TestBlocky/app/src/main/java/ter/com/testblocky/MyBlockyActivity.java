4008b3f9b8ec44b0b4f8302fbcc10c3ed88100a3
Loic Labourbe <loic.labourbe@etu.univ-orleans.fr>
2018-05-09 16:14:14 +0200

    bluetooth
    le bluetooth

--- TestBlocky/app/src/main/java/ter/com/testblocky/MyBlockyActivity.java
+++ TestBlocky/app/src/main/java/ter/com/testblocky/MyBlockyActivity.java
@@ -1,15 +1,25 @@
 package ter.com.testblocky;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.blockly.android.AbstractBlocklyActivity;
 import com.google.blockly.android.codegen.CodeGenerationRequest;
 import com.google.blockly.android.codegen.LanguageDefinition;
 import com.google.blockly.android.codegen.LoggingCodeGeneratorCallback;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.List;
 
+import static android.content.Context.MODE_APPEND;
 
 
 public class MyBlockyActivity extends AbstractBlocklyActivity {
@@ -45,19 +55,25 @@ public class MyBlockyActivity extends AbstractBlocklyActivity {
         return LUA_GENERATORS;
     }
 
-    CodeGenerationRequest.CodeGeneratorCallback mCodeGeneratorCallback =
-            new LoggingCodeGeneratorCallback(this, "LoggingTag");
+    CodeGenerationRequest.CodeGeneratorCallback mCodeGeneratorCallback = new CodeGenerationRequest.CodeGeneratorCallback(){
+        @Override
+        public void onFinishCodeGeneration(String code){
+            System.out.println("Le code");
+            System.out.println(code);
+            try {
+                SaveGeneratdeCode(code);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    };
+            //new LoggingCodeGeneratorCallback(this, "LoggingTag");
 
 
     @Override
     protected CodeGenerationRequest.CodeGeneratorCallback getCodeGenerationCallback() {
-       /* String s = mCodeGeneratorCallback.toString();
-        try {
-            SaveGeneratdeCode(s);
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }*/
+
         return mCodeGeneratorCallback;
     }
 
@@ -72,17 +88,46 @@ public class MyBlockyActivity extends AbstractBlocklyActivity {
         return new LanguageDefinition("lua/lua_compressed.js", "Blockly.Lua");
     }
 
-/* void SaveGeneratdeCode(String code) throws IOException {
-        FileOutputStream out = openFileOutput("test.lua",MODE_APPEND);
-        OutputStreamWriter osw = new OutputStreamWriter(out);
-        osw.write(code.getBytes);
+ void SaveGeneratdeCode(String code) throws IOException {
+
+        System.out.println("code"+code);
+
+        File file = new File("/storage/emulated/0/bluetooth/codeTablette.txt");
+
+        FileOutputStream fos = new FileOutputStream(file);
+
+        fos.write(code.getBytes());
+
+        fos.flush();
+        fos.close();
+
+        /*
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        OutputStreamWriter osw = new OutputStreamWriter(fileOutputStream);
+        osw.write(code);
+
+
+        Toast.makeText(ProgramLoadedActivity.getAppContext(), file.toString(), Toast.LENGTH_SHORT).show();
+
         osw.flush();
-        osw.close();
+        osw.close();*
+         */
+
+       /* File file = new File(ProgramLoadedActivity.getAppContext().getFilesDir(),"codeTablette.txt");
+
+        Toast.makeText(ProgramLoadedActivity.getAppContext(), "path="+file.getPath()+"name="+file.getName(), Toast.LENGTH_SHORT).show();
+
+
+     //OutputStreamWriter osw = new OutputStreamWriter(out);
+        //osw.write(code);
+     // osw.flush();
+     // osw.close();
+        /*
         File path = this.getFilesDir();
         System.out.println(path.toString());
         String chemin = path.toString();
         try {
-            FileInputStream in = openFileInput("test.lua");
+            FileInputStream in = openFileInput("codeTablette.txt");
             InputStreamReader isr = new InputStreamReader(in);
             char [] buffer = new char[2000];
             isr.read(buffer);
@@ -91,7 +136,7 @@ public class MyBlockyActivity extends AbstractBlocklyActivity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     void verifFichier(File path) {
@@ -108,7 +153,7 @@ public class MyBlockyActivity extends AbstractBlocklyActivity {
             e.printStackTrace();
         }
 
-    }*/
+    }
 
 
 }
--- TestBlocky/app/src/main/java/ter/com/testblocky/ProgramLoadedActivity.java
+++ TestBlocky/app/src/main/java/ter/com/testblocky/ProgramLoadedActivity.java
@@ -1,11 +1,25 @@
 package ter.com.testblocky;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
 
 /**
  * Created by Julie on 11/03/2018.
@@ -13,10 +27,16 @@ import android.widget.TextView;
 public class ProgramLoadedActivity extends Activity {
 
     final String PROG_NAME = "program_name";
+    private BluetoothAdapter bluetoothAdapter;
+    public static final java.util.UUID MY_UUID
+            = java.util.UUID.fromString("DEADBEEF-0000-0000-0000-000000000000");
+
+    private static ProgramLoadedActivity instance;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance=this;
         setContentView(R.layout.program_loaded_activity);
 
         Intent intent = getIntent();
@@ -35,6 +55,62 @@ public class ProgramLoadedActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+
+        final Button buttonExec = (Button) findViewById(R.id.executer);
+        buttonExec.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (bluetoothAdapter == null)
+                    Toast.makeText(ProgramLoadedActivity.this, "Pas de Bluetooth",
+                            Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(ProgramLoadedActivity.this, "Avec Bluetooth",
+                            Toast.LENGTH_SHORT).show();
+
+                if (!bluetoothAdapter.isEnabled()) {
+                    bluetoothAdapter.enable();
+                }
+
+                Set<BluetoothDevice> devices;
+                devices = bluetoothAdapter.getBondedDevices();
+                for (BluetoothDevice blueDevice : devices) {
+                    //Toast.makeText(ProgramLoadedActivity.this, "Device = " + blueDevice.getName(), Toast.LENGTH_SHORT).show();
+                    if(blueDevice.getName().equals("pcduino3")){
+                        /*
+                        File file = new File(getApplicationContext().getFilesDir(),"codeTablette.txt");
+                        System.out.println("path="+file.getPath()+"name="+file.getName());
+                        System.out.println("can read="+file.canRead()+"can write="+file.canWrite());
+                        file.setExecutable(true);
+                        file.setReadable(true);
+                        file.setWritable(true);
+                        sendFileToDevice(blueDevice,file);
+                        */
+                        File file2 = new File("/storage/emulated/0/bluetooth/codeTablette.txt");
+                        sendFileToDevice(blueDevice,file2);
+
+                    }
+                }
+
+            }
+
+        });
+    }
+
+    public void sendFileToDevice(BluetoothDevice device,File file){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setClassName("com.android.bluetooth", "com.android.bluetooth.opp.BluetoothOppLauncherActivity");
+        intent.setType("text/plain");
+        intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        startActivity(intent);
+    }
+
+    public static Context getAppContext(){
+        return instance;
     }
 
 }
